e0efc252820bc8c8854ecf4876f27288f3e7c52a
Feby Tanzil <feby.tanzil@shopee.com>
2018-11-16 17:40:08 +0700

    implement reconnection and close

---
+++ client.go
@@ -0,0 +1,50 @@
+package gomailer
+
+import (
+	"errors"
+)
+
+type Implementation int
+
+const (
+	Gomail = Implementation(iota)
+)
+
+type Client interface {
+	// Send will send email
+	Send(msg *Message) error
+	// Close permanently close client connection
+	Close() error
+}
+
+type Message struct {
+	Attachments []*Attachment
+	SendTo      []string
+	CC          []string
+	Title       string
+	Body        string
+	ID          string
+}
+
+type Attachment struct {
+	Filename string
+	Byte     []byte
+}
+
+type Config struct {
+	Host     string
+	Port     int
+	Email    string
+	Password string
+}
+
+var ErrClosed = errors.New("connection has been closed")
+
+// New email return email handler struct
+func NewClient(impl Implementation, emailConfig Config) (Client, error) {
+	if Gomail == impl {
+		return newGomail(&emailConfig), nil
+	}
+
+	return nil, errors.New("no email implementations found")
+}
--- gomail.go
+++ gomail.go
@@ -1,57 +1,156 @@
-package email
+package gomailer
 
 import (
-	"fmt"
 	"gopkg.in/gomail.v2"
 	"io"
+	"sync"
+	"sync/atomic"
 )
 
-type gomailHandler struct {
-	sender gomail.SendCloser
-	config *Config
+type goMail struct {
+	sender      gomail.SendCloser
+	config      *Config
+	isConnected int32
+	isClosed    int32
+	messagePool chan *poolMessage
+	m           sync.Mutex
 }
 
-func newGomail(emailConfig *Config) (*gomailHandler, error) {
-	dialer := gomail.NewPlainDialer("smtp.gmail.com", 587, emailConfig.Email, emailConfig.Password)
-	s, err := dialer.Dial()
+type poolMessage struct {
+	future chan futureError
+	msg    *Message
+}
+
+type futureError struct {
+	err error
+}
+
+const (
+	stateDisconnected = iota
+	stateConnected
+	stateClosed
+)
+
+func newGomail(emailConfig *Config) *goMail {
+	return &goMail{
+		config:      emailConfig,
+		messagePool: make(chan *poolMessage),
+	}
+}
+
+func (h *goMail) Send(msg *Message) error {
+	ftr := make(chan futureError)
+	defer close(ftr)
+
+	m := &poolMessage{
+		future: ftr,
+		msg:    msg,
+	}
+	err := h.sendToPool(m)
 	if nil != err {
-		return nil, err
+		return err
 	}
+	f := <-ftr
 
-	return &gomailHandler{
-		sender: s,
-		config: emailConfig,
-	}, nil
+	return f.err
 }
 
-func (h *gomailHandler) Send(sendEmailTo *MailDetail) error {
-	m := gomail.NewMessage()
-	m.SetHeader("From", h.config.Email)
-	m.SetHeader("To", sendEmailTo.SendTo...)
-	if len(sendEmailTo.SendToCc) > 0 {
-		m.SetHeader("Cc", sendEmailTo.SendToCc...)
+func (h *goMail) disconnect() error {
+	if h.sender == nil {
+		return nil
 	}
-	m.SetHeader("Subject", sendEmailTo.Title)
-	m.SetBody("text/html", sendEmailTo.Body)
+	if !atomic.CompareAndSwapInt32(&h.isConnected, stateConnected, stateDisconnected) {
+		return nil
+	}
 
-	for _, element := range sendEmailTo.Attachment {
-		fileByte := element.Byte
-		m.Attach(element.Filename, gomail.SetCopyFunc(func(w io.Writer) error {
-			_, err := w.Write(fileByte)
-			return err
-		}))
+	return h.sender.Close()
+}
+
+func (h *goMail) Close() error {
+	if h.sender == nil {
+		return nil
 	}
-	if err := gomail.Send(h.sender, m); err != nil {
-		return fmt.Errorf("error sending email: %s", err)
+	h.m.Lock()
+	defer h.m.Unlock()
+
+	if !atomic.CompareAndSwapInt32(&h.isClosed, 0, stateClosed) {
+		return nil
+	}
+
+	if err := h.sender.Close(); nil != err {
+		return err
 	}
 
 	return nil
 }
 
-func (h *gomailHandler) Close() error {
-	if h.sender == nil {
+func (h *goMail) listen() {
+	for task := range h.messagePool {
+		msg := task.msg
+		m := gomail.NewMessage()
+		m.SetHeader("From", h.config.Email)
+		m.SetHeader("To", msg.SendTo...)
+		if len(msg.CC) > 0 {
+			m.SetHeader("Cc", msg.CC...)
+		}
+		m.SetHeader("Subject", msg.Title)
+		m.SetBody("text/html", msg.Body)
+
+		for _, element := range msg.Attachments {
+			fileByte := element.Byte
+			m.Attach(element.Filename, gomail.SetCopyFunc(func(w io.Writer) error {
+				_, err := w.Write(fileByte)
+				return err
+			}))
+		}
+
+		err := gomail.Send(h.sender, m)
+		if err != nil {
+			h.disconnect()
+		}
+
+		go func(e error, task *poolMessage) {
+			task.future <- futureError{
+				err: e,
+			}
+		}(err, task)
+	}
+}
+
+func (h *goMail) connect() error {
+	h.m.Lock()
+	defer h.m.Unlock()
+
+	if stateClosed == atomic.LoadInt32(&h.isClosed) {
+		return ErrClosed
+	}
+	if stateConnected == atomic.LoadInt32(&h.isConnected) {
 		return nil
 	}
 
-	return h.sender.Close()
+	dialer := gomail.NewPlainDialer(h.config.Host, h.config.Port, h.config.Email, h.config.Password)
+	s, err := dialer.Dial()
+	if nil != err {
+		return err
+	}
+
+	h.sender = s
+	atomic.StoreInt32(&h.isConnected, stateConnected)
+	go h.listen()
+
+	return nil
+}
+
+func (h *goMail) sendToPool(task *poolMessage) error {
+	if stateConnected != atomic.LoadInt32(&h.isConnected) {
+		if err := h.connect(); nil != err {
+			return err
+		}
+	}
+
+	go func() {
+		h.messagePool <- task
+	}()
+
+	return nil
 }
